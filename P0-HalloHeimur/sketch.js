9ba85eb1e8ee045f6e9bce30f4ce561e87d7fe7d
Tóti <thorhallurtryggva@gmail.com>
2018-10-17 10:59:39 +0000

    stækkun

--- P0-HalloHeimur/sketch.js
+++ P0-HalloHeimur/sketch.js
@@ -1,14 +1,14 @@
 // Hér kemur kóðinn þinn:
 function setup(){
-    createCanvas(300,300);
+    createCanvas(1080,480);
     background(60,0,60);
     fill(51);
-    ellipse(150,150,100,100);
+    ellipse(530,240,100,100);
     fill(150);
-    ellipse(150,150,50,50);
+    ellipse(530,240,50,50);
     fill(0);
     rect(200,200,10);
-    ellipse(150,150,25,25);
+    ellipse(530,240,25,25);
     textSize(20);
-    text("Halló heimur",95,120);
+    text("Halló heimur",475,340);
 }
