01f0a8988b0e2d1a98515ed8b74318371cb6ff49
1301992679 <1301992679@mh.is>
2018-10-26 13:20:39 +0000

    breyting

---
+++ P1/sketch.js
@@ -0,0 +1,14 @@
+// Hér kemur kóðinn þinn:
+function setup(){
+    createCanvas(1080,480);
+    background(60,0,60);
+    fill(51);
+    ellipse(530,240,100,100);
+    fill(150);
+    ellipse(530,240,50,50);
+    fill(0);
+    rect(200,200,10);
+    ellipse(530,240,25,25);
+    textSize(20);
+    text("Halló heimur",475,340);
+}
---
+++ P2-Andlit/sketch.js
@@ -0,0 +1,43 @@
+// Hér kemur kóðinn þinn:
+var eyeSize = 20;
+var faceWidth = 100;
+var faceHeight = 150;
+var x = 150;
+var y = 150;
+var augah = 150 + faceWidth*0.25;
+var augav = 150 - faceWidth*0.25;
+var munnurl = 255;
+var litur = 0;
+
+
+function setup(){
+  createCanvas (300,300);
+}
+
+function draw(){
+  background(180);
+  fill(0);
+  rect(150-faceWidth/2, 150, faceWidth, faceHeight/2);
+  fill(255);
+  ellipse(x, y, faceWidth, faceHeight);
+  fill(0);
+  arc(x, y, faceWidth, faceHeight, PI, 0);
+  ellipse(augav, height/2, eyeSize, eyeSize);
+  ellipse(augah, height/2, eyeSize, eyeSize);
+  fill(munnurl);
+  fill (litur);
+  arc(x, y+30, faceWidth/2, 30, 0, PI);
+  fill(255);
+  arc(x, y+10, faceWidth/5, faceHeight/4, 0, PI);
+  //.....kóði sem staðsetur augun m.v. x, y, faceWidth og faceHeight ætti að koma hér.
+
+}
+
+function mousePressed(){
+  //Í hvert sinn sem músinni er smellt fá breyturnar ný gildi.
+  faceWidth  = random(75,  150);
+  faceHeight = random(100, 200);
+  eyeSize    = random(10,  30);
+  munnurl 	 = random(0, 255);
+  litur = int(random(-1,1));
+}
