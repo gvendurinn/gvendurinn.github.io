96c3b40703fefbb87ebf18a817d554a0ea9e9fc1
1301992679 <1301992679@mh.is>
2018-10-26 14:01:01 +0000

    geometriskt

--- P1-Geometriskt/sketch.js
+++ P1-Geometriskt/sketch.js
@@ -1,14 +1,20 @@
 // Hér kemur kóðinn þinn:
 function setup(){
-    createCanvas(1080,480);
-    background(60,0,60);
-    fill(51);
-    ellipse(530,240,100,100);
-    fill(150);
-    ellipse(530,240,50,50);
+    createCanvas(900,900);
+    background(255,255,255);
+    strokeWeight(15); // Beastly
+    fill(255,0,0);
+    rect(0,0,400,500);//hyrningar
+    fill(255);
+    rect(0,500,400,400);
+    fill(255);
+    rect(400,0,500,500);
+    fill(0,0,255);
+    rect(825,500,75,400);
+    fill(255);
+    rect(825,650,75,250);
+    fill(255,255,0);
+    rect(400,850,200,50);
     fill(0);
-    rect(200,200,10);
-    ellipse(530,240,25,25);
-    textSize(20);
-    text("Halló heimur",475,340);
-}
+    rect(600,850,225,50);
+  }
