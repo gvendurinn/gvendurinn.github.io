23c2d585015ace1e4b4d4dccacd3da185c19e09d
1301992679 <1301992679@mh.is>
2018-10-31 10:04:30 +0000

    p3c

--- P3c-Lykkjur/sketch.js
+++ P3c-Lykkjur/sketch.js
@@ -1,17 +1,17 @@
 function setup() {
-  createCanvas(300,300);
+  createCanvas(300,400);
   background(100);
   noStroke();
-  frameRate(5);
+  frameRate(10);
   colorMode(HSB);
 }
 
 function draw() {
-  for(var x = 0; x < width; x = x + 10){
-    for(var y = 0; y < height; y = y + 10){
-      for(var z = 0; z < 100; z = z + 10) {
-      fill(random(0,360),360,360);
-      ellipse(x-z,y+z, x-10,y-z);
+  for(var x = 0; x < width; x = x+10){
+    for(var y = 0; y < height; y = y+10){
+      for(var z = 0; z < 300; z = z + 10) {
+      fill(random(0,360),166 ,100);
+      rect(x-z,y+z, tan(sqrt(y+z+x)),tan(z+x+y));
     }
     }
   }
