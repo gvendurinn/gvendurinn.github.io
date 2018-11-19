5fb3e188ab2a39938c7c7cb2c01dcc6322450ea2
1301992679 <1301992679@mh.is>
2018-11-19 12:29:37 +0000

    stebbi

--- P7-Figura/sketch.js
+++ P7-Figura/sketch.js
@@ -1,14 +1,37 @@
-// Hér kemur kóðinn þinn:
-function setup(){
-    createCanvas(1080,480);
-    background(60,0,60);
-    fill(51);
-    ellipse(530,240,100,100);
-    fill(150);
-    ellipse(530,240,50,50);
-    fill(0);
-    rect(200,200,10);
-    ellipse(530,240,25,25);
-    textSize(20);
-    text("Halló heimur",475,340);
+function setup() {
+  createCanvas(300,450);
+
+}
+function draw() {
+  var haus = 10;
+  background(255);
+  stebbi(100,100,haus,10);
+  stebbi(200,200,3*haus,10);
+
+
+}
+function stebbi(x,y, haus,litur){
+  colorMode(HSB);
+  stroke(litur,80,70);
+  noFill();
+  strokeWeight(3);
+  rect(x-haus,y+haus/2,2*haus, 2*haus);
+  ellipse(x-haus/2,y+3*haus,haus,haus);
+  ellipse(x+haus/2,y+3*haus,haus,haus);
+  ellipse(x-haus/2,y+4*haus,haus,2*haus);
+  ellipse(x+haus/2,y+4*haus,haus,2*haus);
+  ellipse(x-haus/2,y+5*haus,haus,2*haus);
+  ellipse(x+haus/2,y+5*haus,haus,2*haus);
+  ellipse(x,y,haus,haus);
+  ellipse(x-haus/2,y+6*haus,haus,haus/2);
+  ellipse(x+haus/2,y+6*haus,haus,haus/2);
+  ellipse(x+haus,y+haus,haus,haus);
+  ellipse(x-haus,y+haus,haus,haus);
+  ellipse(x+haus,y+3*haus/2,haus,1.5*haus);
+  ellipse(x+haus,y+5*haus/2,0.75*haus,1.5*haus);
+  ellipse(x+1.2*haus,y+3.5*haus,0.5*haus,0.5*haus);
+  ellipse(x-haus,y+3*haus/2,haus,1.5*haus);
+  ellipse(x-haus,y+5*haus/2,0.75*haus,1.5*haus);
+  ellipse(x-1.2*haus,y+3.5*haus,0.5*haus,0.5*haus);
+
 }
