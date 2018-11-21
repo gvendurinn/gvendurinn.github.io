69db3b0724533331b7039da6ac57d5ae143a6707
1301992679 <1301992679@mh.is>
2018-11-21 09:41:19 +0000

    stebbist

--- P7-Figura/sketch.js
+++ P7-Figura/sketch.js
@@ -1,37 +1,42 @@
+var size;
+
 function setup() {
   createCanvas(300,450);
-
+  size = random(0.4,1.2);
 }
 function draw() {
   var haus = 10;
   background(255);
-  stebbi(100,100,haus,10);
-  stebbi(200,200,3*haus,10);
-
+  stebbi(100,100,3*haus,10,size);
+  stebbi(200,300,2*haus,10,1)
+  stebbi(250,200,1.5*haus,10,0.7);
+  stebbi(100,300,2*haus,10,0.5);
 
 }
-function stebbi(x,y, haus,litur){
+function stebbi(x,y,haus,litur,test){
   colorMode(HSB);
   stroke(litur,80,70);
   noFill();
   strokeWeight(3);
-  rect(x-haus,y+haus/2,2*haus, 2*haus);
-  ellipse(x-haus/2,y+3*haus,haus,haus);
-  ellipse(x+haus/2,y+3*haus,haus,haus);
-  ellipse(x-haus/2,y+4*haus,haus,2*haus);
-  ellipse(x+haus/2,y+4*haus,haus,2*haus);
-  ellipse(x-haus/2,y+5*haus,haus,2*haus);
-  ellipse(x+haus/2,y+5*haus,haus,2*haus);
+  rectMode(CENTER);
+  rect(x,y+1.5*haus,2*haus*test, 2*haus);
+  ellipse(x-haus/2,y+3*haus,test*haus,haus);
+  ellipse(x+haus/2,y+3*haus,test*haus,haus);
+  ellipse(x-haus/2,y+4*haus,test*haus,2*haus);
+  ellipse(x+haus/2,y+4*haus,test*haus,2*haus);
+  ellipse(x-haus/2,y+5*haus,test*haus,2*haus);
+  ellipse(x+haus/2,y+5*haus,test*haus,2*haus);
   ellipse(x,y,haus,haus);
   ellipse(x-haus/2,y+6*haus,haus,haus/2);
   ellipse(x+haus/2,y+6*haus,haus,haus/2);
-  ellipse(x+haus,y+haus,haus,haus);
-  ellipse(x-haus,y+haus,haus,haus);
-  ellipse(x+haus,y+3*haus/2,haus,1.5*haus);
-  ellipse(x+haus,y+5*haus/2,0.75*haus,1.5*haus);
-  ellipse(x+1.2*haus,y+3.5*haus,0.5*haus,0.5*haus);
-  ellipse(x-haus,y+3*haus/2,haus,1.5*haus);
-  ellipse(x-haus,y+5*haus/2,0.75*haus,1.5*haus);
-  ellipse(x-1.2*haus,y+3.5*haus,0.5*haus,0.5*haus);
+
+  ellipse(x+haus*test+5,y+haus,test*haus,haus);
+  ellipse(x-haus*test-5,y+haus,test*haus,haus);
+  ellipse(x+haus*test+5,y+3*haus/2,test*haus,1.5*haus);
+  ellipse(x+test*haus+5,y+5*haus/2,0.75*haus*test,1.5*haus);
+  ellipse(x+1.2*haus*test,y+3.5*haus,0.5*haus,0.5*haus);
+  ellipse(x-haus*test-5,y+3*haus/2,test*haus,1.5*haus);
+  ellipse(x-test*haus-5,y+5*haus/2,0.75*haus*test,1.5*haus);
+  ellipse(x-1.2*haus*test,y+3.5*haus,0.5*haus,0.5*haus);
 
 }
